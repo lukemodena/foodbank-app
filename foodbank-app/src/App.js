f8d4a1f35bb5bf395f43bd854a62af485de806eb
lukemodena <97751351+lukemodena@users.noreply.github.com>
2022-08-15 12:35:55 +0100

    modified:   foodbank-app/public/index.html 	modified:   foodbank-app/src/App.js

--- foodbank-app/src/App.js
+++ foodbank-app/src/App.js
@@ -4,10 +4,8 @@ import {Home} from './Home';
 import {Donor} from './Components/Donor/Donor';
 import {Collection} from './Components/Collection/Collection';
 import {Navigation} from './Navigation';
-import { Participation } from './Components/Donor/ParticipationDemo';
 
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import { default as ImageDemo } from './Components/ImageDemo/ImageDemo';
 
 function App() {
   return (
@@ -23,8 +21,6 @@ function App() {
           <Route exact path='/' element={<Home/>}/>
           <Route path='/donor' element={<Donor/>}/>
           <Route path='/collection' element={<Collection/>}/>
-          <Route path='/imagedemo' element={<ImageDemo/>}/>
-          <Route path='/participationdemo' element={<Participation/>}/>
         </Routes>
       </div>
     </BrowserRouter>
